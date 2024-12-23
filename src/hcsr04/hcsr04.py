8236e7e1353ef56a70744f8c7b99993251c9a2e5
DustinJoosen <dustinjoosen2003@gmail.com>
2024-12-23 17:13:26 +0100

    Button, Buzzer & HCSR

--- src/hcsr04/hcsr04.py
+++ src/hcsr04/hcsr04.py
@@ -1,35 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
-HCSR04_OUT = 17
-HCSR04_IN = 4
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(HCSR04_OUT, GPIO.OUT)
-GPIO.setup(HCSR04_IN, GPIO.IN)
+class Hcsr:
+    # Pin configuration.
+    HCSR04_OUT = 27
+    HCSR04_IN = 17
 
-def get_distance():    
-    GPIO.output(HCSR04_OUT, GPIO.HIGH)
-    time.sleep(0.00001)
-    GPIO.output(HCSR04_OUT, GPIO.LOW)
+    def __init__(self):
+        GPIO.setup(self.HCSR04_OUT, GPIO.OUT)
+        GPIO.setup(self.HCSR04_IN, GPIO.IN)
 
-    while GPIO.input(HCSR04_IN) == GPIO.LOW:
-        start = time.time()
-        print("s")
-        
-    while GPIO.input(HCSR04_IN) == GPIO.HIGH:
-        end = time.time()
-        print("e")
-        exit()
-
-    sig_time = end - start
-    distance = sig_time / 0.000058
+    def get_distance_cm(self):
+        GPIO.output(self.HCSR04_OUT, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(self.HCSR04_OUT, GPIO.LOW)
     
-    return distance
+        while GPIO.input(self.HCSR04_IN) == GPIO.LOW:
+            start = time.time()
+        
+        while GPIO.input(self.HCSR04_IN) == GPIO.HIGH:
+            end = time.time()
+    
+        sig_time = end - start
+        distance = sig_time / 0.000058
+        time.sleep(0.1)
+        return distance
 
-try:
-    print(get_distance())
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    GPIO.cleanup()
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+    hcsr = Hcsr()
+    
+    while True:
+        print(hcsr.get_distance_cm())        
+
--- src/main.py
+++ src/main.py
@@ -1,23 +1,33 @@
 from camera.camera_display import CameraDisplay
-import time
+from button.button import Button
+from buzzer.buzzer import Buzzer
+from hcsr04.hcsr04 import Hcsr
+import RPi.GPIO as GPIO
 
-def camera_display_n_seconds(n):
-    start_time = time.time()
+# Set the GPIO mode to BCM.
+GPIO.setmode(GPIO.BCM)
+
+
+def main():
+    # Initialize the sensors
     camera_display = CameraDisplay()
+    button = Button()
+    buzzer = Buzzer()
+    hcsr = Hcsr()
 
+    # On button presses, do the following events:
+    button.add_callback(lambda _: buzzer.buzz_in_ring_pattern()) 
+    button.add_callback(lambda _: camera_display.capture_and_display_n_secs(30))
 
+    # Main loop. Keep the program running.
     while True:
-        camera_display.capture_and_display()
-        camera_display.apply_facial_recognition()
-        camera_display.send_buffer_to_api()
-        
-        if time.time() - start_time >= n and n != -1:
-            print(f"Done with displaying {n} times :)")
-            break
+        distance = hcsr.get_distance_cm()
+        print(f"Current distance measured from hcsr: {distance}")
 
-    camera_display.cleanup()
-
-
-while True:
-    secs = int(input("how many seconds should it turn on?\n>>>"))
-    camera_display_n_seconds(-1)
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        GPIO.cleanup()
